56a82a6678141a5149d630b4df848f2146bfffb2
samuel santos <samaraujo28@gmail.com>
2022-10-01 03:51:04 -0300

    test: implement password validator test

---
+++ app/backend/src/tests/unit-password-validator.spec.ts
@@ -0,0 +1,22 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+import { Password } from '../entities';
+// @ts-ignore
+const { expect } = chai;
+
+describe('Password validator', () => {
+  it('should return true when an valid password is pass by parameters', () => {
+    const password =  'my_secret_password';
+    const sut = Password.validate(password);
+
+    expect(sut).to.be.equal(true);
+  });
+
+  it('should return false when password length is less than 6 characters', () => {
+    const password = 'my_s'
+    const sut = Password.validate(password);
+
+    expect(sut).to.be.equal(false);
+  });
+});
