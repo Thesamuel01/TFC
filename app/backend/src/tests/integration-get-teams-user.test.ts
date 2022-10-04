095685b4903496db4ff22fed2cdb30898f50b9d6
samuel santos <samaraujo28@gmail.com>
2022-10-04 16:37:23 -0300

    test: implement get teams integration test

---
+++ app/backend/src/tests/integration-get-teams-user.test.ts
@@ -0,0 +1,98 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Team from '../database/models/Team';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Get teams integration test', () => {
+  describe('/teams', () => {
+    let chaiHttpResponse: Response;
+
+    before(async () => {
+      sinon
+        .stub(Team, "findAll")
+        .resolves([{id: 1, teamName: 'Avaí/Kindermann'}, { id: 2, teamName: 'BahiaVasco' }] as Team[]);
+    });
+
+    after(()=>{
+      (Team.findAll as sinon.SinonStub).restore();
+    })
+
+    it('should return an status code 200 and all teams', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams');
+
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.a('array');
+      expect(chaiHttpResponse.body[0]).to.have.property('id', 1);
+      expect(chaiHttpResponse.body[0]).to.have.property('teamName', 'Avaí/Kindermann');
+    });
+  });
+
+  describe('/teams/:id', () => {
+    let chaiHttpResponse: Response;
+    let stub: sinon.SinonStub;
+
+    beforeEach(async () => {
+      stub = sinon.stub(Team, "findByPk")
+    });
+
+    afterEach(()=>{
+      (Team.findByPk as sinon.SinonStub).restore();
+    });
+
+    it('should return a status code 200 and a team',async () => {
+      stub.resolves({id: 1, teamName: 'Avaí/Kindermann'} as Team);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/1');
+
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.have.property('id', 1);
+      expect(chaiHttpResponse.body).to.be.have.property('teamName', 'Avaí/Kindermann');
+    });
+  
+    it('should return a status code 400 and an error message id param is not a number',async () => {
+      stub.resolves({id: 1, teamName: 'Avaí/Kindermann'} as Team);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/test')
+
+
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.body)
+        .to.be.have.property('message')
+        .to.be.a('string')
+        .to.be.equal('Request param must be a number');
+    });
+
+
+    it('should return a status code 404 and an error message when team is not found',async () => {
+      stub.resolves(null);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/90');
+
+
+      expect(chaiHttpResponse.status).to.be.equal(404);
+      expect(chaiHttpResponse.body)
+        .to.be.have.property('message')
+        .to.be.a('string')
+        .to.be.equal('Team not found');
+    });
+  });
+});
