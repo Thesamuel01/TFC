c508a90d092fef0d762fc45869e67df2d88e654a
samuel santos <samaraujo28@gmail.com>
2022-10-04 14:51:33 -0300

    test: implement team test

---
+++ app/backend/src/tests/unit-team.spec.ts
@@ -0,0 +1,19 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+import Team, { TeamData } from '../entities/team';
+// @ts-ignore
+const { expect } = chai;
+
+describe('Team domain entity', () => {
+  it('should able to create a team', () => {
+    const teamData: TeamData = {
+      id: 1,
+      name: 'Vasco'
+    }
+
+    const team = Team.create(teamData);
+
+    expect(team).to.be.an.instanceof(Team);
+  });
+});
