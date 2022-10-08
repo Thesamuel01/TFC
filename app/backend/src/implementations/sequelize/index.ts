0bf5d9d9f81e1aea9e015c618e35567d46346675
samuel santos <samaraujo28@gmail.com>
2022-10-07 23:28:30 -0300

    feat: implement sequelize match repository

--- app/backend/src/implementations/sequelize/index.ts
+++ app/backend/src/implementations/sequelize/index.ts
@@ -1,7 +1,9 @@
 import SequelizeUserReposiroty from './sequelize-user-repository';
 import SequelizeTeamRpository from './sequelize-team-repository';
+import SequelizeMatchReposiroty from './sequlize-match-repository';
 
 export {
   SequelizeUserReposiroty,
   SequelizeTeamRpository,
+  SequelizeMatchReposiroty,
 };
---
+++ app/backend/src/implementations/sequelize/sequlize-match-repository.ts
@@ -0,0 +1,40 @@
+import { FindAllParams } from '../../repositories/match-repository';
+import MatchModel from '../../database/models/Match';
+import Team from '../../database/models/Team';
+import { MatchDataDTO, UpdateMatchDTO, CreateMatchDTO } from '../../DTOs';
+import { MatchRepository } from '../../repositories';
+
+export default class SequelizeMatchReposiroty implements MatchRepository {
+  constructor(private model = MatchModel) {}
+
+  async findAll(params?: FindAllParams): Promise<MatchDataDTO[]> {
+    const data = await this.model.findAll({
+      ...params,
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    return data;
+  }
+
+  async findByPk(id: number): Promise<MatchDataDTO | null> {
+    return this.model.findByPk(id);
+  }
+
+  async update({
+    id,
+    homeTeamGoals,
+    awayTeamGoals,
+  }: UpdateMatchDTO): Promise<void> {
+    await this.model.update(
+      { homeTeamGoals, awayTeamGoals },
+      { where: { id } },
+    );
+  }
+
+  async create(data: CreateMatchDTO): Promise<MatchDataDTO> {
+    return this.model.create(data);
+  }
+}
