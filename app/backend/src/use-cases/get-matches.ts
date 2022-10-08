59d59ea4bf38ac03df031e26c5a32c916a374918
samuel santos <samaraujo28@gmail.com>
2022-10-07 23:26:09 -0300

    feat: create get matches use case

---
+++ app/backend/src/use-cases/get-matches.ts
@@ -0,0 +1,23 @@
+import { MatchDataDTO } from '../DTOs';
+import { MatchRepository } from '../repositories';
+
+type QueryString = {
+  inProgress: boolean
+};
+
+export default class GetMatches {
+  constructor(
+    private matchesRepository: MatchRepository,
+  ) {}
+
+  async execute(data?: QueryString): Promise<MatchDataDTO[]> {
+    if (data) {
+      const { inProgress } = data;
+      const params = { where: { inProgress } };
+
+      return this.matchesRepository.findAll(params);
+    }
+
+    return this.matchesRepository.findAll();
+  }
+}
