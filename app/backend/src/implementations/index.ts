f72e38499bbdb73f432ae6683196a8f644534325
samuel santos <samaraujo28@gmail.com>
2022-10-10 02:54:11 -0300

    chore: update index file

--- app/backend/src/implementations/index.ts
+++ app/backend/src/implementations/index.ts
@@ -1,9 +1,15 @@
 import { loginUserImplementation } from './login-user-implementation';
 import { getTeamsImplementation } from './get-tems-implementation';
 import { getMatchesImplementation } from './get-matches-implementation';
+import { createMatchImplementation } from './create-match-implementation';
+import authControllerImplementation from './auth-controller-implementation';
+import { updateMatchImplementation } from './update-match-implementation';
 
 export {
   getTeamsImplementation,
   loginUserImplementation,
   getMatchesImplementation,
+  createMatchImplementation,
+  authControllerImplementation,
+  updateMatchImplementation,
 };
