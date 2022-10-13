9025b50bb908d22fcf1d41dbeeb63aec60c149ee
samuel santos <samaraujo28@gmail.com>
2022-10-12 21:43:25 -0300

    refact: refact match entity

--- app/backend/src/entities/match.ts
+++ app/backend/src/entities/match.ts
@@ -2,11 +2,22 @@ import Team from './team';
 
 export type MatchData = {
   id?: number;
-  homeTeam: Team;
+  homeTeamId: number;
+  homeTeamName: string;
   homeTeamGoals: number;
-  awayTeam: Team;
+  awayTeamId: number;
+  awayTeamName: string;
   awayTeamGoals: number;
   inProgress: boolean;
+};
+
+export type MatchAttr = {
+  id?: number;
+  homeTeam: Team;
+  awayTeam: Team;
+  homeTeamGoals: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
 };
 
 export type UpdateMatchData = {
@@ -15,40 +26,59 @@ export type UpdateMatchData = {
 };
 
 export default class Match {
-  public readonly id: number;
-  public readonly homeTeam: Team;
+  public readonly id?: number;
   public readonly homeTeamGoals: number;
-  public readonly awayTeam: Team;
   public readonly awayTeamGoals: number;
   public readonly inProgress: boolean;
+  private readonly _homeTeam: Team;
+  private readonly _awayTeam: Team;
 
-  private constructor(props: MatchData) {
+  public get homeTeam() {
+    return this._homeTeam.id;
+  }
+
+  public get awayTeam() {
+    return this._awayTeam.id;
+  }
+
+  public get teamHome() {
+    const { teamName } = this._homeTeam;
+
+    return { teamName };
+  }
+
+  public get teamAway() {
+    const { teamName } = this._awayTeam;
+
+    return { teamName };
+  }
+
+  private constructor(props: MatchAttr) {
     if (props.id) {
       this.id = props.id;
     }
 
-    this.homeTeam = Team.create({
-      id: props.homeTeam.id,
-      name: props.homeTeam.teamName,
-    });
-    this.awayTeam = Team.create({
-      id: props.awayTeam.id,
-      name: props.awayTeam.teamName,
-    });
-
+    this._homeTeam = props.homeTeam;
+    this._awayTeam = props.awayTeam;
     this.homeTeamGoals = props.homeTeamGoals;
     this.awayTeamGoals = props.awayTeamGoals;
     this.inProgress = props.inProgress;
   }
 
-  static create(props: MatchData) {
-    return new Match({
-      id: props.id,
-      homeTeam: props.homeTeam,
-      homeTeamGoals: props.awayTeamGoals,
-      awayTeam: props.awayTeam,
-      awayTeamGoals: props.awayTeamGoals,
-      inProgress: props.inProgress,
-    });
+  static create({
+    id,
+    homeTeamId,
+    homeTeamName,
+    homeTeamGoals,
+    awayTeamId,
+    awayTeamName,
+    awayTeamGoals,
+    inProgress,
+
+  }: MatchData): Match {
+    const homeTeam = Team.create({ id: homeTeamId, name: homeTeamName });
+    const awayTeam = Team.create({ id: awayTeamId, name: awayTeamName });
+
+    return new Match({ id, homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress });
   }
 }
